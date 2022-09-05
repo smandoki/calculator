5b3ef00a7680e051fa71450b04d5580e5e85c2e5
smandoki <80867148+smandoki@users.noreply.github.com>
2022-09-05 16:32:00 +0800

    Complete core logic
    Implemented all of the javascript and core logic

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ function multiply(a, b) {
 
 function divide(a, b) {
     if (b === 0) {
-        return 'UNDEFINED';
+        return 'nice try...';
     }
 
     return a / b;
@@ -34,3 +34,71 @@ function operate (a, b, op) {
     }
 }
 
+//vars for storing current working values
+let memory = 0;
+let operation;
+let wipe = false;
+
+//place digits pressed onto display
+const display = document.querySelector('#display');
+const digits = document.querySelectorAll('.digits');
+
+for (let digit of digits) {
+    digit.addEventListener('click', (e) => {
+        if (wipe) {
+            display.innerText = 0;
+            wipe = false;
+        }
+
+        if (display.innerText === '0') {
+            display.innerText = e.target.id;
+        } else {
+            display.innerText += e.target.id;
+        }
+    });
+}
+
+
+//clear display and memory
+const clear = document.querySelector('#ac');
+clear.addEventListener('click', () => {
+    display.innerText = 0;
+    memory = 0;
+    operation = null;
+});
+
+
+//handle math operations
+const ops = document.querySelectorAll('.operation');
+
+for (let op of ops) {
+    op.addEventListener('click', (e) => {
+        wipe = true;
+
+        if (operation) {
+            evaluate();
+            wipe = false;
+        }
+
+        memory = Number(display.innerText);
+        operation = e.target.id;
+        
+        if (wipe) display.innerText = 0;
+
+        wipe = true;
+    });
+}
+
+
+//handle equals press
+const equals = document.querySelector('#equal');
+equals.addEventListener('click', evaluate);
+
+function evaluate() {
+    if (operation === null) return;
+
+    display.innerText = operate(memory, Number(display.innerText), operation);
+    operation = null;
+
+    wipe = true;
+}
